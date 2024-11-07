e74e5fbc79384f6e4237cc56def2ff81470fa8cf
asp <asp@aptech.com>
2024-11-07 20:43:16 +0500

    auth file done

---
+++ middleware/auth.js
@@ -0,0 +1,21 @@
+const jwt = require("jsonwebtoken")
+
+
+
+const authMiddleware = (req, res, next) => {
+    const token = req.header("token");
+
+    if(!token){
+        return res.status(401).json({error: "token not present"})
+    }
+
+    try {
+        const decodedToken = jwt.verify(token,process.env.MONGO_URI);
+        req.userId = decodedToken.id;
+        next();
+    } catch (error) {
+        return res.status(401).json({error: "invalid token"})
+    }
+}
+
+module.exports = authMiddleware
